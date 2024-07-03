7c57f2f0a5436ca9540fce9fdd878410e7e4af95
vknanavati <vimin.nanavati@gmail.com>
2024-07-03 17:31:28 -0400

    first commit.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const API_KEY = '3d7a17012bef44deac1453d0f9b74f19'
+
+function App() {
+
+  const [city, setCity] = useState("")
+
+  const handleCity = e => {
+    setCity(e.target.value)
+  }
+
+  const handleClick = () => {
+    console.log(city)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&units=I&key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  })
+  return (
+    <>
+      <h1>Weather App</h1>
+      <div>
+        <input
+          id="city"
+          type="text"
+          placeholder="enter city"
+          value={city}
+          onChange={handleCity}
+        />
+        <button
+          onClick={handleClick}
+        >Search</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
